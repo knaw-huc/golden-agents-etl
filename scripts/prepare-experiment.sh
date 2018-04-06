0d9d02a5cdfd476433235aa33ab84c78a66adeaf
Ubuntu <datamanager@reconciliation-test.em1pdpm0svfu1j4rifq0r4pqlh.ax.internal.cloudapp.net>
2018-04-06 09:36:05 +0000

    fixed script

--- scripts/prepare-experiment.sh
+++ scripts/prepare-experiment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# ./prepare-experiment.sh saaMariageTest 001 002
+# ./scripts/prepare-experiment.sh test 8081 001 002
+# or to just run lenticular lenses:
+# ./scripts/prepare-experiment.sh test 8081
+
 
 set -o nounset
 set -o errexit
@@ -11,9 +14,10 @@ server_port="$1"
 shift
 
 while (( "$#" )); do
-  name="basename input/rdf/SAA-ID-$#*"
-  /opt/stardog/bin/stardog data add http://localhost:8120/${stardog_database_name} -g "http://goldenagents.org/datasets/${name}" input/rdf/${name}/*.ttl  
+  name=`basename input/rdf/SAA-ID-${1}*`
+  echo "Uploading $name to stardog"
+  /opt/stardog/bin/stardog data add http://localhost:8120/${stardog_database_name} -g "http://goldenagents.org/datasets/${name}" input/rdf/${name}/*.ttl
   shift
 done
 
-LL_STARDOG_DATABASE="${stardog_database_name}" LL_PORT="${server_port}" ~/alignments/src/run.py
+LL_STARDOG_DATABASE="${stardog_database_name}" LL_PORT="${server_port}" python ~/alignments/src/run.py
